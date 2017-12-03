d44293c03711adacf19b59c6577c54f396081934
Bagicode <bagicode@gmail.com>
2017-12-03 07:32:12 +0700

    how to easy create generete setter and getter

---
+++ app/src/main/java/com/bagicode/www/myhome/Model/Model.java
@@ -0,0 +1,42 @@
+package com.bagicode.www.myhome.Model;
+
+/**
+ * Created by bagicode on 03/12/17.
+ */
+
+public class Model {
+
+    private String image;
+    private int id;
+
+    public Model() {
+    }
+
+    public Model(String image, int id) {
+        this.image = image;
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // how to call func generete ?
+    // in linux you can alt + insert
+    // you can try now :D
+    // super easy
+
+    // bye :D
+}
